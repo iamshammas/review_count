ab922b5a48f76a66f36f0fe1fd4e72036ad9f215
SHAMMAS <muhdshammas612@gmail.com>
2026-02-17 00:18:07 +0530

    some changes on settings

--- review_count/settings.py
+++ review_count/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#wgy@%6@qu^39m=u^xubtif(uyt#3q3(=(5q2u63eof4k0()82'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
